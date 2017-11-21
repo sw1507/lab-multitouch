01a611a994cb412db279ce1e4b7e9c4d5a4a53a4
Su Wang <sw1507@uw.edu>
2017-11-20 23:38:30 -0800

    finish

--- app/src/main/java/edu/uw/multitouchlab/MainActivity.java
+++ app/src/main/java/edu/uw/multitouchlab/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 
+/**
+ * 
+ */
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "Main";
